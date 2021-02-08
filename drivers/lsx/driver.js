cf1a322b2ec697862c1766845a23cca89450466b
Harwin Borger <harwin@studioborger.nl>
2021-02-08 22:26:12 +0100

    Add basic to show KEF LSX for pairing

--- drivers/lsx/driver.js
+++ drivers/lsx/driver.js
@@ -7,7 +7,32 @@ class LSXDriver extends Homey.Driver {
    * onInit is called when the driver is initialized.
    */
   async onInit() {
-    this.log('MyDriver has been initialized');
+    this.log('KEF LSX driver has been initialized');
+  }
+
+  async onPair(session) {
+    const devices = [
+      {
+        name: 'KEF LSX',
+        data: {
+          id: 'kef-lsx',
+        },
+      },
+    ];
+
+    session.setHandler('list_devices', async function (data) {
+      // emit when devices are still being searched
+      session.emit('list_devices', devices);
+
+      // return devices when searching is done
+      return devices;
+
+      // when no devices are found, return an empty array
+      // return [];
+
+      // or throw an Error to show that instead
+      // throw new Error('Something bad has occured!');
+    });
   }
 
   /**
@@ -15,7 +40,7 @@ class LSXDriver extends Homey.Driver {
    * This should return an array with the data of devices that are available for pairing.
    */
   async onPairListDevices() {
-    return [
+    const devices = [
        {
          name: 'KEF LSX',
          data: {
@@ -23,6 +48,8 @@ class LSXDriver extends Homey.Driver {
          },
        },
     ];
+
+    return devices;
   }
 }
 
